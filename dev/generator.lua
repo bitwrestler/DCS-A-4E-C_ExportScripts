f3c6643c24e8aae864c544ed43ef2abbc3203bb1
Eric Spencer <eric@spenc.org>
2023-11-05 18:51:16 -0500

    WIP genertor (throwing wierd error)

--- dev/generator.lua
+++ dev/generator.lua
@@ -2,7 +2,39 @@
 require("generator_globals")
 require("clickabledata")
 
+local cmdMap = {
+    "%1d",
+    "%.1f",
+    "%1d",
+    "%.1f",
+    "%.1f",
+    "%.1f",
+    "%.1f",
+    "%.1f"
+}
+
+function convertControl(cmd,inc,rng)
+    if(inc % 1 > 0) then 
+        if ((rng[1] % .1) > 0) then 
+            return "%.1f" 
+        else 
+            return "%.2f" 
+        end 
+    else
+        return "%1d"
+    end
+end
+
 for k,v in pairs(elements) do
-    io.write("[",v.arg[1], "] = \"%.f\"  -- ", v.hint, " (Device ", v.device, " Command ", v.action[1], " ID ", v.arg[1] , ")")
-    break
+    local dcsid = v.arg[1]
+    local desc = v.hint
+    local device = v.device
+    local command = v.action[1]
+    local increment = v.arg_value[1]
+    local range = v.arg_lim[1]
+
+    local numeric_type = convertControl(command, increment, range)
+
+    io.write("[",dcsid, "] = \"",numeric_type,"\"  -- ", desc, " (Device ", device, " Command ", command, " ID ", dcsid , " Increment ",increment," Range ",range[1],"-",range[2],")")
+    print()
 end

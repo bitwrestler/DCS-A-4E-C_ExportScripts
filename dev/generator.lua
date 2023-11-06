855a22d09cf1d352508662cd03096eeef9c7e25e
Eric Spencer <eric@spenc.org>
2023-11-05 19:11:03 -0500

    non-erroring generator

--- dev/generator.lua
+++ dev/generator.lua
@@ -13,12 +13,33 @@ local cmdMap = {
     "%.1f"
 }
 
-function convertControl(cmd,inc,rng)
-    if(inc % 1 > 0) then 
-        if ((rng[1] % .1) > 0) then 
-            return "%.1f" 
+local function isNumeric(v)
+    return type(v) == "number"
+end
+
+local function ifnil(val)
+    if(val == nil) then
+        return ""
+    else
+        return val
+    end
+end
+
+local function ifNilFirstEle(tableVal)
+    if(tableVal == nil or tableVal[1] == nil) then
+       return ""
+    else
+        return tableVal[1]
+    end
+end
+
+
+local function convertControl(cmd,inc,rng)
+    if(isNumeric(inc) and (inc % 1 > 0) ) then
+        if ((rng[1] % .1) > 0) then
+            return "%.1f"
         else 
-            return "%.2f" 
+            return "%.2f"
         end 
     else
         return "%1d"
@@ -26,15 +47,17 @@ function convertControl(cmd,inc,rng)
 end
 
 for k,v in pairs(elements) do
+    if(v.arg == nil or v.arg[1] == nil) then
+        goto mycontinue
+    end
     local dcsid = v.arg[1]
-    local desc = v.hint
-    local device = v.device
-    local command = v.action[1]
-    local increment = v.arg_value[1]
-    local range = v.arg_lim[1]
+    local desc = ifnil(v.hint)
+    local device = ifnil(v.device)
+    local command = ifNilFirstEle(v.action)
+    local increment = ifNilFirstEle(v.arg_value)
+    local range = ifNilFirstEle(v.arg_lim)
 
     local numeric_type = convertControl(command, increment, range)
-
-    io.write("[",dcsid, "] = \"",numeric_type,"\"  -- ", desc, " (Device ", device, " Command ", command, " ID ", dcsid , " Increment ",increment," Range ",range[1],"-",range[2],")")
-    print()
+    io.write("[",dcsid, "] = \"",numeric_type,"\"  -- ", desc, " (Device ", device, " Command/Button ", command, " ID ", dcsid , " Increment ",increment," Range ",range[1],"-",range[2],")","\r\n")
+    ::mycontinue::
 end

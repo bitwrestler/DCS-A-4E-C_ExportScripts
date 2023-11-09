d565420561ec50d4ad158f672642cc76e625fca6
Eric Spencer <eric@spenc.org>
2023-11-08 21:38:14 -0500

    add names of commands and ids to generator comment

--- dev/generator.lua
+++ dev/generator.lua
@@ -54,23 +54,46 @@ local function table_invert(t)
     return s
  end
 
+local function lookupElementNameByID(eleTable, idVal)
+    for k,v in pairs(eleTable) do
+        if( v.arg[1] == idVal) then
+            return k
+        end
+    end
+    return ""
+end
+
 local invert_devices = table_invert(devices)
+local invert_commands = table_invert(device_commands)
+local invert_keys = table_invert(Keys)
+
+local function lookupCommandNameByID(idVal)
+    local poss = invert_commands[idVal]
+    if(poss == nil) then
+        poss = invert_keys[idVal]
+    end
+    return poss
+end
+
+
 local grpbyDeviceTable = {}
 for k,v in pairs(elements) do
     if(v.arg == nil or v.arg[1] == nil or v.device == nil) then
         goto mycontinue
     end
     local dcsid = v.arg[1]
+    local dcsid_name = lookupElementNameByID(elements, dcsid)
     local device = ifnil(v.device)
 
     local desc = ifnil(v.hint)
     local device_name = invert_devices[device]
     local command = ifNilFirstEle(v.action)
+    local command_name = lookupCommandNameByID(command)
     local increment = ifNilFirstEle(v.arg_value)
     local range = ifNilFirstEle(v.arg_lim)
 
     local numeric_type = convertControl(command, increment, range)
-    local defintionStr = "["..dcsid.. "] = \""..numeric_type.."\",  -- ".. desc.. " (Device "..device_name.. "/".. device.. " Command/Button ".. command.. " ID ".. dcsid .. " Increment "..increment.." Range "..range[1].."-"..range[2]..")"
+    local defintionStr = "["..dcsid.. "] = \""..numeric_type.."\",  -- ".. desc.. " (Device "..device_name.. "/".. device.. " Command/Button ".. command_name .. "/".. command.. " ID ".. dcsid_name .. "/" .. dcsid .. " Increment "..increment.." Range "..range[1].."-"..range[2]..")"
     
     if grpbyDeviceTable[device] == nil then grpbyDeviceTable[device] = {} end
     grpbyDeviceTable[device][#grpbyDeviceTable[device]+1] =  defintionStr
